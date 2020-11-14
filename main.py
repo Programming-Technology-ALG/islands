bebf4aa884c94bb94d2a84727bc915dcfe8a7aca
alexgiving <alexgiving@mail.ru>
2020-11-14 20:26:36 +0300

    testing

--- main.py
+++ main.py
@@ -29,50 +29,50 @@
 # увеличиваем количество островов
 
 
-def max_arr() -> int:
-    max_el = -1
-    for ind in range(N):
-        if array[0][ind] != '0':
-            max_el = max(max_el, array[0][ind])
-    return max_el
+# def max_arr() -> int:
+#     max_el = -1
+#     for ind in range(N):
+#         if array[0][ind] != '0':
+#             max_el = max(max_el, array[0][ind])
+#     return max(max_el, num - 1)
 
 
 N = int(input())
 array = [[0 for _ in range(N)] for _ in range(2)]
 num = 1
-for k in range(N):
-    if k == 0:              # Считываю 1 строку
-        array[0] = list(input())
-        for j in range(N):
-            if array[0][j] == 'x':
-                array[0][j] = num
-                if j + 1 < N:
-                    if array[0][j + 1] == 'x':
-                        array[0][j + 1] = array[0][j]
-                    else:
-                        num += 1
-                        j += 1
-                    continue
-                num += 1
-    else:                   # Считываю остальные строки в array[1]
-        array[1] = list(input())
-        if array[1] == ['0' for _ in range(N)]:
-            break
+array[0] = list(input())
+for j in range(N):
+    if array[0][j] == 'x':
+        array[0][j] = num
+        num += 1
+        if j + 1 < N:
+            if array[0][j + 1] == 'x':
+                array[0][j + 1] = array[0][j]
+            else:
+                j += 1
+for _ in range(N - 1):  # Считываю остальные строки в array[1]
+    array[1] = list(input())
+    if array[1] == ['0' for _ in range(N)]:
+        break
+    for j in range(N):
         temp_arr = []
         temp_num = num
-        for j in range(N):
-            for p in range(j, N):
-                if array[1][p] == 'x':
-                    temp_arr.append(p)
-                else:
-                    j += p
-                    break
+        for p in range(j, N):
+            if array[1][p] == 'x':
+                temp_arr.append(p)
+            else:
+                break
+        if len(temp_arr) != 0:
             for index in temp_arr:
                 if array[0][index] != '0':
                     temp_num = min(array[0][index], temp_num)
+                if array[0][index] == num - 1:
+                    num -= 1
             for index in temp_arr:
                 array[1][index] = temp_num
+            if temp_num == num:
+                num += 1
+    array[0] = array[1]  # Переставляю строку на уровень выше
 
-        array[0] = array[1]     # Переставляю строку на уровень выше
-
-print(max_arr())
+# print(max_arr())
+print(num - 1)
